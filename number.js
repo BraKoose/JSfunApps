70db4303f974be4bc425642852c016e1da6de637
Koose <KooseTheBlogger@gmail.com>
2021-03-26 21:17:46 +0000

    Koose undertand2

---
+++ number.js
@@ -0,0 +1,13 @@
+function koo(){
+    var a = 10;
+    function se(){
+        let b = 20;
+            function koose(){
+                let = 30;
+            console.log(a, b, c);
+        }se()
+        console.log(a, b);
+    } koose();
+    console.log(a);
+}
+koo();
