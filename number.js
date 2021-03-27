ceb28b0caf1b8db64ab0da311343fb1c672f6ae2
Koose <KooseTheBlogger@gmail.com>
2021-03-27 11:43:16 +0000

    I can"

--- number.js
+++ number.js
@@ -1,13 +1,12 @@
-function koo(){
-    var a = 10;
-    function se(){
-        let b = 20;
-            function koose(){
-                let = 30;
-            console.log(a, b, c);
-        }se()
-        console.log(a, b);
-    } koose();
-    console.log(a);
-}
-koo();
+var salvation = "Hello";
+var greeting =`${salvation},
+    You 
+    Can 
+    You
+    Must
+    You
+    Should
+    Be
+    Hunger!`        
+
+console.log(greeting);
